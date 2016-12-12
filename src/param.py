335c5fc66fad86c376e43d5c5dc4008b07bc9172
Shoto <hermestrain@hotmail.co.jp>
2016-12-12 23:56:12 +0900

    Add param.py
    param.py convert digital param made by GA to concrete param for DL.

--- src/param.py
+++ src/param.py
@@ -12,12 +12,36 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 
-class Test:
+class Param:
     def __init__(self):
         pass
 
 
     def main(self):
+        param = []
+        
+        param.append(random.randint(0, 3))  # 0: Output Weight
+        param.append(random.randint(0, 3))  # 1: Output Standard deviation
+        param.append(random.randint(0, 1))  # 2: Output Bias
+        param.append(random.randint(0, 3))  # 3: Output Activation Function
+        param.append(random.randint(0, 1))  # 4: Train Optimaize
+        param.append(random.randint(0, 3))  # 5: Learning Rate
+        param.append(random.randint(0, 2))  # 6: Batch Size
+        param.append(random.randint(0, 4))  # 7: The Number of Iteration
+        param.append(random.randint(0, 3))  # 8: The Number of hidden layer
+        
+        # 9~: Hidden Layer Design
+        idx = 8
+        for i in range(param[-1]):
+            param.append(random.randint(0, 2))  # The Number of Node
+            param.append(random.randint(0, 3))  # Output Weight
+            param.append(random.randint(0, 3))  # Output Standard deviation
+            param.append(random.randint(0, 1))  # Output Bias
+            param.append(random.randint(0, 3))  # Output Activation Function
+        
+
+
+    def convert_param(self, param):
         """
         # FizzBuzz
         X  = tf.placeholder(tf.float32, [None, n_X])
@@ -112,52 +136,8 @@ class Test:
             print '%-12s'%k, dic[k]
 
 
-    def accuracy(self):
-        """
-        Prediction Accracy Calculation Test.
-        accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1)), tf.float32))
-        """
-        Y = np.array([
-                        [0.1, 0.2, 0.3, 0.4],
-                        [0.0, 0.8, 0.2, 0.0],
-                        [0.0, 0.4, 0.5, 0.1]
-                    ])
-
-        print Y
-
-        Y_ = np.array([
-                        [0.0, 0.0, 1.0, 0.0],
-                        [0.0, 1.0, 0.0, 0.0],
-                        [0.0, 0.0, 1.0, 0.0]
-                    ])
-
-        print Y_
-
-        sess = tf.Session()
-        
-        print sess.run(tf.argmax(Y, 1))
-
-        print sess.run(tf.argmax(Y_, 1))
-
-
-        eq = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
-        print sess.run(eq)
-
-        print sess.run(tf.cast(eq, tf.float32))
-
-        print sess.run(tf.reduce_mean(tf.cast(eq, tf.float32)))
-
-        accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1)), tf.float32))
-        print sess.run(accuracy)
-
-
-        result = np.mean(sess.run(tf.argmax(Y, 1))==sess.run(tf.argmax(Y_, 1)))
-        print sess.run(tf.argmax(Y, 1))==sess.run(tf.argmax(Y_, 1))
-        print result
-
-
 
 if __name__ == "__main__":
-    Test().main()
+    Param().main()
 
 
