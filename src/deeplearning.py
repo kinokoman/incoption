ed526a83ee364adaed83a73edfe1f333c2e8c507
Shoto <hermestrain@hotmail.co.jp>
2017-01-08 00:40:04 +0900

    Add param generator to deeplearnig.py
    It will cooperate with genetic algorithm later.

--- src/deeplearning.py
+++ src/deeplearning.py
@@ -6,87 +6,20 @@ import numpy as np
 from collections import Counter
 import tensorflow as tf
 import time
+import datetime
 
 from data_fizzbuzz import DataFizzBuzz
 from data_mnist import DataMnist
+from param import Param
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-STDDEV = 0.01
-#LEARNING_RATE = 0.05 # FizzBuzz
-LEARNING_RATE = 0.01 # MNIST for beginner
-BATCH_SIZE = 10 # 
-#N_ITER = 10000 # FizzBuzz
-N_ITER = 10 # MNIST for beginner
 LOG_PATH = '../log/'
-
 
 
 class DeepLearning:
     def __init__(self):
-        """
-        - layer
-            - n_node: 5 ~ 100
-            - weight: 'zeros', 'ones', 'random_normal', 'truncated_normal'
-            - stddev: 0.0001 ~ 0.1
-            - bias: 'zeros', 'ones'
-            - activ_func: '', 'relu', 'tanh', 'softmax'
-
-        - trainer
-            - optimizer: GradientDescentOptimizer, AdamOptimizer
-            - learning rate: 0.0001 ~ 0.1
-
-        - batch size: 10 ~ 100
-        - num iter: 1 ~ 10000
-
-
-        param = [1, ]
-
-         0: output weight: 0, 1, 2, 3
-         1: output stddev: 0.0001, 0.001, 0.01, 0.1
-         2: output bias: 0, 1
-         3: output activatin function: 0, 1, 2, 3
-         4: train optimizer: 0, 1
-         5: learning rate: 0.0001, 0.001, 0.01, 0.1
-         6: batch size: 10, 50, 100
-         7: num iter: 1, 10, 100, 1000, 100000
-         8: num hidden layer: 0, 1, 2, 3
-        ---------------------------------------------
-         9: num node: 50
-        10: hidden weight: 0, 1, 2, 3
-        11: hidden stddev: 0.0001, 0.001, 0.01, 0.1
-        12: hidden bias: 0, 1
-        13: hidden activatin function: 0, 1, 2, 3
-        
-        dic['o_weight'] = param[0]
-        dic['o_stddev'] = param[1]
-        dic['o_bias']   = param[2]
-        dic['o_activ']  = param[3]
-
-        dic['tr_opt']     = param[4]
-        dic['tr_rate']    = param[5]
-        dic['batch_size'] = param[6]
-        dic['n_iter']     = param[7]
-        dic['n_h_layer']  = param[8]
-
-        idx = 8
-        for i in range(param[idx]):
-            dic['h%s_n_node'%(i+1)] = param[idx+1+5*i]
-            dic['h%s_weight'%(i+1)] = param[idx+2+5*i]
-            dic['h%s_stddev'%(i+1)] = param[idx+3+5*i]
-            dic['h%s_bias'%(i+1)]   = param[idx+4+5*i]
-            dic['h%s_activ'%(i+1)]  = param[idx+5+5*i]
-                
-        # FizzBuzz
-        X  = tf.placeholder(tf.float32, [None, n_X])
-        H1 = self.__make_layer(X, n_X, n_hidden[0], 'random_normal', 'zeros', 'relu')
-        Y  = self.__make_layer(H1, n_hidden[0], n_Y, 'random_normal', 'zeros', '')
-        Y_ = tf.placeholder(tf.float32, [None, n_Y])
-        """
-
-        
-
         pass
 
 
@@ -94,17 +27,137 @@ class DeepLearning:
         start = time.time()
 
         print 'Setting data...'
-        #data = DataFizzBuzz().main()
-        data = DataMnist().main()
+        data = DataFizzBuzz().main()
+        #data = DataMnist().main()
         
         print 'Training...'
-        model = self.design_model(data)
-        self.train_model(data, model)
+        seq_nums = Param().generate_random_seq_nums()
+        params = Param().convert_param(seq_nums)
+
+        model = self.design_network(data, params)
+        self.train_network(data, model, params)
 
         end = time.time()
         print (end-start)/60, 'minutes trained model.'
 
 
+    def design_network(self, data, params):
+        # Set input/output size.
+        n_X = data[0].shape[1]
+        n_Y = data[1].shape[1]
+
+        # Make network from params
+        if params.has_key('h3_n_node'):
+            pass
+        elif params.has_key('h2_n_node'):
+            pass
+        elif params.has_key('h1_n_node'):
+            X  = tf.placeholder(tf.float32, [None, n_X])
+            H1 = self.__make_layer(X, n_X, params['h1_n_node'], params['h1_weight'], params['h1_stddev'], params['h1_bias'], params['h1_activ'])
+            Y  = self.__make_layer(H1, params['h1_n_node'], n_Y, params['o_weight'], params['o_stddev'], params['o_bias'], params['o_activ'])
+            Y_ = tf.placeholder(tf.float32, [None, n_Y])
+        else:
+            pass        
+
+        # Select trainer
+        loss, train_step = self.__select_trainer(Y, Y_, params)
+
+        network = {'X': X, 'Y': Y, 'Y_': Y_, 'loss': loss, 'step': train_step}
+
+        return network
+
+
+    def __make_layer(self, I, n_input, n_output, weight, std_dev, bias, activ):
+        # Weight
+        if weight == 'zeros':
+            W = tf.Variable(tf.zeros([n_input, n_output]))
+        elif weight == 'ones':
+            W = tf.Variable(tf.ones([n_input, n_output]))
+        elif weight == 'random_normal':
+            W = tf.Variable(tf.random_normal([n_input, n_output], stddev=std_dev))
+        elif weight == 'truncated_normal':
+            W = tf.Variable(tf.truncated_normal([n_input, n_output], stddev=std_dev))
+        
+        # Bias
+        if bias == 'zeros':
+            B = tf.Variable(tf.zeros([n_output]))
+        elif bias == 'ones':
+            B = tf.Variable(tf.ones([n_output]))
+
+        # Activation Function
+        if activ == '':
+            O = tf.matmul(I, W) + B
+        elif activ == 'relu':
+            O = tf.nn.relu(tf.matmul(I, W) + B)
+        elif activ == 'tanh':
+            O = tf.nn.tanh(tf.matmul(I, W) + B)
+        elif activ == 'softmax':
+            O = tf.nn.softmax(tf.matmul(I, W) + B)
+
+        return O
+
+
+    def __select_trainer(self, Y, Y_, params):
+        if params['tr_opt'] == 'GradientDescentOptimizer':
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(Y, Y_))
+            train_step = tf.train.GradientDescentOptimizer(params['tr_rate']).minimize(loss)
+        elif params['tr_opt'] == 'AdamOptimizer':
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(Y, Y_))
+            train_step = tf.train.AdamOptimizer(params['tr_rate']).minimize(loss)
+
+        return loss, train_step
+
+
+    def train_network(self, data, network, params):
+        # data
+        train_data  = data[0]
+        train_label = data[1]
+        test_data  = data[2]
+        test_label = data[3]
+
+        # model
+        X  = network['X']
+        Y  = network['Y']
+        Y_ = network['Y_']
+        loss  = network['loss']
+        step  = network['step']
+
+        # Setting
+        sess = tf.InteractiveSession()
+        tf.initialize_all_variables().run()
+        accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1)), tf.float32))
+        
+        logs = []
+        for epoch in range(params['n_iter']+1):
+            # Randamize data
+            p = np.random.permutation(range(len(train_data)))
+            train_data, train_label = train_data[p], train_label[p]
+
+            # Training
+            for start in range(0, train_label.shape[0], params['batch_size']):
+                end = start + params['batch_size']
+                sess.run(step, feed_dict={X: train_data[start:end], Y_: train_label[start:end]})
+            
+            # Testing
+            train_loss = sess.run(loss, feed_dict={X: train_data, Y_: train_label})
+            train_accuracy = sess.run(accuracy, feed_dict={X: train_data, Y_: train_label})
+            test_accuracy = sess.run(accuracy, feed_dict={X: test_data, Y_: test_label})
+
+            # Logging
+            log = {'epoch': epoch, 'train_loss': train_loss, 'train_accuracy': train_accuracy, 'test_accuracy': test_accuracy}
+            logs.append(log)
+
+            if epoch % 100 == 0:
+                std_output = 'Epoch: %s, \t Train Loss: %s, \t Train Accuracy: %s, \t Test Accuracy: %s'
+                print(std_output % (log['epoch'], log['train_loss'], log['train_accuracy'], log['test_accuracy']))
+
+        # Save logs
+        dt = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        df = pd.DataFrame(logs)
+        df.to_csv("./log/accuracy_and_error_%s.csv"%dt, index=False)
+                
+
+    """
     def design_model(self, data):
         # Set parameters.
         n_X = data[0].shape[1]
@@ -120,21 +173,17 @@ class DeepLearning:
         Y  = self.__make_layer(H2, n_hidden[1], n_Y, 'random_normal', 'zeros', '')
         Y_ = tf.placeholder(tf.float32, [None, n_Y])
         
-        """
         # FizzBuzz
         X  = tf.placeholder(tf.float32, [None, n_X])
         H1 = self.__make_layer(X, n_X, n_hidden[0], 'random_normal', 'zeros', 'relu')
         Y  = self.__make_layer(H1, n_hidden[0], n_Y, 'random_normal', 'zeros', '')
         Y_ = tf.placeholder(tf.float32, [None, n_Y])
-        """
-
-        """
+        
         # MNIST for beginner
         X  = tf.placeholder(tf.float32, [None, n_X])
         Y = self.__make_layer(X, n_X, n_Y, 'zeros', 'zeros', '')
         Y_ = tf.placeholder(tf.float32, [None, n_Y])
-        """
-
+        
         loss, train_step = self.__select_trainer(Y, Y_, 0)  # FizzBuzz, MNIST for beginner
 
         model = {'X': X, 'Y': Y, 'Y_': Y_, 'loss': loss, 'train_step': train_step}
@@ -217,14 +266,15 @@ class DeepLearning:
                     accu_test = sess.run(accuracy, feed_dict={X: test_X, Y_: test_Y})
                     print start, lo55, accu_train, accu_test
 
-            """
             # Test
             if epoch % 100 == 0:
                 lo55 = sess.run(loss, feed_dict={X: train_X, Y_: train_Y})
                 accu_train = sess.run(accuracy, feed_dict={X: train_X, Y_: train_Y})
                 accu_test = sess.run(accuracy, feed_dict={X: test_X, Y_: test_Y})
                 print epoch, lo55, accu_train, accu_test
-            """
+
+        """
+
 
 
 if __name__ == "__main__":
--- src/param.py
+++ src/param.py
@@ -19,113 +19,120 @@ class Param:
 
 
     def main(self):
-        param = []
+        pass
+
+
+    def generate_random_seq_nums(self):
+        seq_nums = []
         
-        param.append(random.randint(0, 3))  # 0: Output Weight
-        param.append(random.randint(0, 3))  # 1: Output Standard deviation
-        param.append(random.randint(0, 1))  # 2: Output Bias
-        param.append(random.randint(0, 0))  # 3: Output Activation Function
-        param.append(random.randint(0, 1))  # 4: Train Optimaize
-        param.append(random.randint(0, 3))  # 5: Learning Rate
-        param.append(random.randint(0, 2))  # 6: Batch Size
-        param.append(random.randint(0, 4))  # 7: The Number of Iteration
-        param.append(random.randint(0, 3))  # 8: The Number of hidden layer
+        seq_nums.append(random.randint(0, 3))  # 0: Output Weight
+        seq_nums.append(random.randint(0, 3))  # 1: Output Standard deviation
+        seq_nums.append(random.randint(0, 1))  # 2: Output Bias
+        seq_nums.append(random.randint(0, 0))  # 3: Output Activation Function
+        seq_nums.append(random.randint(0, 1))  # 4: Train Optimaize
+        seq_nums.append(random.randint(0, 3))  # 5: Learning Rate
+        seq_nums.append(random.randint(0, 2))  # 6: Batch Size
+        seq_nums.append(random.randint(0, 4))  # 7: The Number of Iteration
+        seq_nums.append(random.randint(1, 1))  # 8: The Number of hidden layer
         
         # 9~: Hidden Layer Design
         idx = 8
-        for i in range(param[-1]):
-            param.append(random.randint(0, 2))  # The Number of Node
-            param.append(random.randint(0, 3))  # Output Weight
-            param.append(random.randint(0, 3))  # Output Standard Deviation
-            param.append(random.randint(0, 1))  # Output Bias
-            param.append(random.randint(0, 3))  # Output Activation Function
+        for i in range(seq_nums[-1]):
+            seq_nums.append(random.randint(0, 2))  # The Number of Node
+            seq_nums.append(random.randint(0, 3))  # Output Weight
+            seq_nums.append(random.randint(0, 3))  # Output Standard Deviation
+            seq_nums.append(random.randint(0, 1))  # Output Bias
+            seq_nums.append(random.randint(0, 3))  # Output Activation Function
 
-        self.convert_param(param)        
+        return seq_nums
 
 
-    def convert_param(self, param):
-        #param = [2, 1, 0, 0, 0, 1, 2, 4, 1, 2, 2, 1, 0, 1]
-        dic = {}
+    def convert_param(self, seq_nums):
+        params = {}
 
         # 0: Output Weight
-        if   param[0] == 0: dic['o_weight'] = 'zeros'
-        elif param[0] == 1: dic['o_weight'] = 'ones'
-        elif param[0] == 2: dic['o_weight'] = 'random_normal'
-        elif param[0] == 3: dic['o_weight'] = 'truncated_normal'
+        if   seq_nums[0] == 0: params['o_weight'] = 'zeros'
+        elif seq_nums[0] == 1: params['o_weight'] = 'ones'
+        elif seq_nums[0] == 2: params['o_weight'] = 'random_normal'
+        elif seq_nums[0] == 3: params['o_weight'] = 'truncated_normal'
 
         # 1: Output Standard deviation
-        if   param[1] == 0: dic['o_stddev'] = 0.1
-        elif param[1] == 1: dic['o_stddev'] = 0.01
-        elif param[1] == 2: dic['o_stddev'] = 0.001
-        elif param[1] == 3: dic['o_stddev'] = 0.0001
+        if   seq_nums[1] == 0: params['o_stddev'] = 0.1
+        elif seq_nums[1] == 1: params['o_stddev'] = 0.01
+        elif seq_nums[1] == 2: params['o_stddev'] = 0.001
+        elif seq_nums[1] == 3: params['o_stddev'] = 0.0001
         
         # 2: Output Bias
-        if   param[2] == 0: dic['o_bias'] = 'zeros'
-        elif param[2] == 1: dic['o_bias'] = 'ones'
+        if   seq_nums[2] == 0: params['o_bias'] = 'zeros'
+        elif seq_nums[2] == 1: params['o_bias'] = 'ones'
         
         # 3: Output Activation Function
-        if   param[3] == 0: dic['o_activ'] = ''
+        if   seq_nums[3] == 0: params['o_activ'] = ''
 
         # 4: Train Optimaize
-        if   param[4] == 0: dic['tr_opt'] = 'GradientDescentOptimizer'
-        elif param[4] == 1: dic['tr_opt'] = 'AdamOptimizer'
+        if   seq_nums[4] == 0: params['tr_opt'] = 'GradientDescentOptimizer'
+        elif seq_nums[4] == 1: params['tr_opt'] = 'AdamOptimizer'
         
         # 5: Learning Rate
-        if   param[5] == 0: dic['tr_rate'] = 0.1
-        elif param[5] == 1: dic['tr_rate'] = 0.01
-        elif param[5] == 2: dic['tr_rate'] = 0.001
-        elif param[5] == 3: dic['tr_rate'] = 0.0001
+        if   seq_nums[5] == 0: params['tr_rate'] = 0.1
+        elif seq_nums[5] == 1: params['tr_rate'] = 0.01
+        elif seq_nums[5] == 2: params['tr_rate'] = 0.001
+        elif seq_nums[5] == 3: params['tr_rate'] = 0.0001
                 
         # 6: Batch Size
-        if   param[6] == 0: dic['batch_size'] = 10
-        elif param[6] == 1: dic['batch_size'] = 50
-        elif param[6] == 2: dic['batch_size'] = 100
+        if   seq_nums[6] == 0: params['batch_size'] = 10
+        elif seq_nums[6] == 1: params['batch_size'] = 50
+        elif seq_nums[6] == 2: params['batch_size'] = 100
         
         # 7: The Number of Iteration
-        if   param[7] == 0: dic['n_iter'] = 1
-        elif param[7] == 1: dic['n_iter'] = 10
-        elif param[7] == 2: dic['n_iter'] = 100
-        elif param[7] == 3: dic['n_iter'] = 1000
-        elif param[7] == 4: dic['n_iter'] = 10000
+        if   seq_nums[7] == 0: params['n_iter'] = 1
+        elif seq_nums[7] == 1: params['n_iter'] = 10
+        elif seq_nums[7] == 2: params['n_iter'] = 100
+        elif seq_nums[7] == 3: params['n_iter'] = 1000
+        elif seq_nums[7] == 4: params['n_iter'] = 10000
 
         # 8: The Number of hidden layer
-        if   param[8] == 0: dic['n_h_layer'] = 0
-        elif param[8] == 1: dic['n_h_layer'] = 1
-        elif param[8] == 2: dic['n_h_layer'] = 2
-        elif param[8] == 3: dic['n_h_layer'] = 3
+        if   seq_nums[8] == 0: params['n_h_layer'] = 0
+        elif seq_nums[8] == 1: params['n_h_layer'] = 1
+        elif seq_nums[8] == 2: params['n_h_layer'] = 2
+        elif seq_nums[8] == 3: params['n_h_layer'] = 3
         
         # 9~: Hidden Layer Design
         idx = 8
-        for i in range(dic['n_h_layer']):
+        for i in range(params['n_h_layer']):
             # The Number of Node
-            if   param[idx+1+5*i] == 0: dic['h%s_n_node'%(i+1)] = 10
-            elif param[idx+1+5*i] == 1: dic['h%s_n_node'%(i+1)] = 50
-            elif param[idx+1+5*i] == 2: dic['h%s_n_node'%(i+1)] = 100
+            if   seq_nums[idx+1+5*i] == 0: params['h%s_n_node'%(i+1)] = 10
+            elif seq_nums[idx+1+5*i] == 1: params['h%s_n_node'%(i+1)] = 50
+            elif seq_nums[idx+1+5*i] == 2: params['h%s_n_node'%(i+1)] = 100
 
             # Output Weight
-            if   param[idx+2+5*i] == 0: dic['h%s_n_weight'%(i+1)] = 'zeros'
-            elif param[idx+2+5*i] == 1: dic['h%s_n_weight'%(i+1)] = 'ones'
-            elif param[idx+2+5*i] == 2: dic['h%s_n_weight'%(i+1)] = 'random_normal'
-            elif param[idx+2+5*i] == 3: dic['h%s_n_weight'%(i+1)] = 'truncated_normal'
+            if   seq_nums[idx+2+5*i] == 0: params['h%s_weight'%(i+1)] = 'zeros'
+            elif seq_nums[idx+2+5*i] == 1: params['h%s_weight'%(i+1)] = 'ones'
+            elif seq_nums[idx+2+5*i] == 2: params['h%s_weight'%(i+1)] = 'random_normal'
+            elif seq_nums[idx+2+5*i] == 3: params['h%s_weight'%(i+1)] = 'truncated_normal'
 
             # Output Standard deviation
-            if   param[idx+3+5*i] == 0: dic['h%s_stddev'%(i+1)] = 0.1
-            elif param[idx+3+5*i] == 1: dic['h%s_stddev'%(i+1)] = 0.01
-            elif param[idx+3+5*i] == 2: dic['h%s_stddev'%(i+1)] = 0.001
-            elif param[idx+3+5*i] == 3: dic['h%s_stddev'%(i+1)] = 0.0001
+            if   seq_nums[idx+3+5*i] == 0: params['h%s_stddev'%(i+1)] = 0.1
+            elif seq_nums[idx+3+5*i] == 1: params['h%s_stddev'%(i+1)] = 0.01
+            elif seq_nums[idx+3+5*i] == 2: params['h%s_stddev'%(i+1)] = 0.001
+            elif seq_nums[idx+3+5*i] == 3: params['h%s_stddev'%(i+1)] = 0.0001
             
             # Output Bias
-            if   param[idx+4+5*i] == 0: dic['h%s_bias'%(i+1)] = 'zeros'
-            elif param[idx+4+5*i] == 1: dic['h%s_bias'%(i+1)] = 'ones'
+            if   seq_nums[idx+4+5*i] == 0: params['h%s_bias'%(i+1)] = 'zeros'
+            elif seq_nums[idx+4+5*i] == 1: params['h%s_bias'%(i+1)] = 'ones'
             
             # Output Activation Function
-            if   param[idx+5+5*i] == 0: dic['h%s_activ'%(i+1)] = ''
-            elif param[idx+5+5*i] == 1: dic['h%s_activ'%(i+1)] = 'relu'
-            elif param[idx+5+5*i] == 2: dic['h%s_activ'%(i+1)] = 'tanh'
-            elif param[idx+5+5*i] == 3: dic['h%s_activ'%(i+1)] = 'softmax'
+            if   seq_nums[idx+5+5*i] == 0: params['h%s_activ'%(i+1)] = ''
+            elif seq_nums[idx+5+5*i] == 1: params['h%s_activ'%(i+1)] = 'relu'
+            elif seq_nums[idx+5+5*i] == 2: params['h%s_activ'%(i+1)] = 'tanh'
+            elif seq_nums[idx+5+5*i] == 3: params['h%s_activ'%(i+1)] = 'softmax'
 
-        for k in sorted(dic):
-            print '%-12s'%k, dic[k]
+        # Dudeg
+        for k in sorted(params):
+            print '%-12s'%k, params[k]
+        print
+
+        return params
 
 
 
